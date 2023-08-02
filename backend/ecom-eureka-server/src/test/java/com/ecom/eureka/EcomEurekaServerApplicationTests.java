2eb42e75bb895ab79c7a200d1bcefab6924dea86
vaibhav-jadhav0512 <vaibhav-jadhav0512>
2023-08-02 19:33:35 +0530

    Initialise Eureka server

---
+++ backend/ecom-eureka-server/src/test/java/com/ecom/eureka/EcomEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
