6226b0b9b857d4592a80e93f638596332f41f6ab
vaibhav-jadhav0512 <vaibhav-jadhav0512>
2023-08-04 12:57:02 +0530

    Authentication implemented

--- backend/ecom-api-gateway/src/main/java/com/ecom/api/gateway/EcomApiGatewayApplication.java
+++ backend/ecom-api-gateway/src/main/java/com/ecom/api/gateway/EcomApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.ecom.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EcomApiGatewayApplication {
 
 	public static void main(String[] args) {
--- backend/ecom-product-service/src/main/java/com/ecom/product/proxy/OrderServiceProxy.java
+++ backend/ecom-product-service/src/main/java/com/ecom/product/proxy/OrderServiceProxy.java
@@ -3,7 +3,7 @@ package com.ecom.product.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "ECOM-ORDER-SERVICE", fallback = OrderServiceFallback.class)
+@FeignClient(name = "ECOM-API-GATEWAY", url = "http://localhost:8888", fallback = OrderServiceFallback.class)
 public interface OrderServiceProxy {
 
 	@GetMapping("/orders/test")
